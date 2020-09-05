567490605e186c284ab8aa568ae36afbe72dcfc0
Daniel Martinez Pascual <daniellopascuallo@hotmail.com>
2020-09-05 09:21:46 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Hamsters from './components/Hamsters';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// converting App function to a class function: we need a render() method that returns something return() with a <div><div/> where all info must be included
+
+// If we want a state e need a constructor() method
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hamsters: []
+    }
+  }
+
+    render(){
+      return(
+        <div>
+          {/* Add Component with tag (in one line, or more clear like below) and add the props to this component with key:value pairs (similar to how it was in html) and remember that if we pass something it is not a string we need curly braces (flow of data) */}
+          <Hamsters 
+            name="Chuckles"
+            color="Brown"
+            age={2}
+            // we can pass a boolean of course:
+            hasWheel={true}
+          />
+          <Hamsters 
+            name="George"
+            color="Light Grey"
+            age={4}
+            hasWheel={false}
+          />
+          <Hamsters 
+            name="Tickles"
+            color="Blue"
+            age={5}
+            hasWheel={true}
+          />
+
+          {/* To do this this way we do not need React, this is not the React way. Creating a components folder and a Component.jsx file */}
+            {/* <div>
+              <h2>Name: Chuckles</h2>
+              <p>Fur Color: Brown</p>
+              <p>Age: 2</p>
+              <p>Has Wheel: Yes</p>
+            </div>
+            <div>
+              <h2>Name: George</h2>
+              <p>Fur Color: Light Grey</p>
+              <p>Age: 4</p>
+              <p>Has Wheel: No</p>
+            </div>
+            <div>
+              <h2>Name: Tickles</h2>
+              <p>Fur Color: Blue</p>
+              <p>Age: 5</p>
+              <p>Has Wheel: Yes</p>
+            </div> */}
+        </div>
+      );
+    }
 }
 
 export default App;
